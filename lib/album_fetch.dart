09a8774423bcbfb697f902b7d3dbff1503e43616
Supermjork <markehabaziz@gmail.com>
2023-12-26 20:23:27 +0200

    embty tile

--- lib/album_fetch.dart
+++ lib/album_fetch.dart
@@ -4,24 +4,23 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 Future<List<Arrival>> fetchArrivals() async {
-  final response = await http
-      .get(Uri.parse('http://141.144.238.26:48502/arrivals/all'));
+  try {
+    final response = await http.get(Uri.parse('http://141.144.238.26:48502/arrivals/all'));
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    List<dynamic> data = jsonDecode(response.body);
-    List<Arrival> posts = List<Arrival>.from(data.map((dynamic postJson) {
-      return Arrival.fromJson(postJson);
-    }));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load Arrival');
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<Arrival> arrivals = List<Arrival>.from(data.map((dynamic arrivalJson) {
+        return Arrival.fromJson(arrivalJson);
+      }));
+      return arrivals;
+    } else {
+      throw Exception('Failed to load Arrival');
+    }
+  } catch (error) {
+    throw Exception('Failed to fetch data: $error');
   }
-
-  throw Exception("API Unavailable");
 }
+
 
 class Arrival {
   final String timeOfDay;
@@ -29,7 +28,7 @@ class Arrival {
   final int tramLine;
   final bool direction;
 
-  const Arrival({
+  Arrival({
     required this.timeOfDay,
     required this.weekDay,
     required this.tramLine,
@@ -37,21 +36,12 @@ class Arrival {
   });
 
   factory Arrival.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'time_of_day': String jsonTime,
-        'week_day': int jsonDay,
-        'tram_line': int jsonTramLine,
-        'direction': bool jsonDirec,
-      } =>
-        Arrival(
-          // Class : json declaration
-          timeOfDay: jsonTime,
-          weekDay: jsonDay,
-          tramLine: jsonTramLine,
-          direction: jsonDirec,
-        ),
-      _ => throw const FormatException('Failed to load Arrival.'),
-    };
+    return Arrival(
+      timeOfDay: json['time_of_day'] as String,
+      weekDay: json['week_day'] as int,
+      tramLine: json['tram_line'] as int,
+      direction: json['direction'] as bool,
+    );
   }
 }
+
